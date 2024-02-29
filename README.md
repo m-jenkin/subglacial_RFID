072563c0518eb16965f40a98b1d0423abd6b1278
m-jenkin <matthewjenkin@protonmail.com>
2024-02-29 07:36:51 +0100

    knitr script render

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ Author: Matt Jenkin, University of Lausanne, Switzerland ([mjenkin\@unil.ch](mai
 
 Date: Feb 2024
 
-Code and data used for the main analysis in the Journal of Glaciology article: [Tracking coarse sediment in an Alpine subglacial channel using radio-tagged particles](https://doi.org/10.1017/jog.2023.77).
+Code and data used for the main analysis in the Journal of Glaciology article: [Tracking coarse sediment in an Alpine subglacial channel using radio-tagged particles](https://doi.org/10.1017/jog.2023.77). This repo represents a major refactoring and cleanup from the original versions (1.0-1.2) published on [Zenodo](https://zenodo.org/doi/10.5281/zenodo.7550558), aiming to aid generalisation to other projects.
 
-Written in R mainly using the `tidyverse` package collection, `sf` for spatial data and `eks` for tidy/geospatial implementation of `ks` kernel smoothing functions. An `.Rproj` and a [`renv`](https://rstudio.github.io/renv/articles/renv.html) lockfile are provided for package version management. This repo represents a major refactoring and cleanup from the original versions (1.0-1.2) published on [Zenodo](https://zenodo.org/doi/10.5281/zenodo.7550558), aiming to aid generalisation to other projects. The code demonstrates the steps used to localise particles with roving antenna and stationary antenna data. A subsequent paper (currently in prep) will contain an automated approach to modelling particle transport distances by uniting data from the two antenna systems. 
+The `run.html` file displays the code and rendered output. Written in R mainly using the `tidyverse` package collection, `sf` for spatial data and `eks` for tidy/geospatial implementation of `ks` kernel smoothing functions. A `.Rproj` and a [`renv`](https://rstudio.github.io/renv/articles/renv.html) lockfile are provided for package version management. The code demonstrates the steps used to localise particles with roving antenna and stationary antenna data. A subsequent paper (currently in prep) will contain an automated approach to modelling particle transport distances by uniting data from the two antenna systems.
 
 ------------------------------------------------------------------------
 
--- run.R
+++ run.R
@@ -1,24 +1,36 @@
-# Packages and data -------------------------------------------------------
+#' ---
+#' title: Tracking coarse sediment in an Alpine subglacial channel
+#' author: Matt Jenkin
+#' date: Feb 2024
+#' format:
+#'   html:
+#'     self-contained: true
+#' execute:
+#'  warning: false
+#' ---
 
 pacman::p_load(tools, readr, tidyr, dplyr, lubridate, purrr, ggplot2, scales, sf, ks, eks)
+#'
+#| include: false
 files_delim <- list.files("./data", pattern = ".csv", full.names = T) |>
   walk(~ assign(file_path_sans_ext(basename(.x)), read_csv(.x), envir = .GlobalEnv))
 files_spatial <- list.files("./data", pattern = ".gpkg", full.names = T) |>
   walk(~ assign(file_path_sans_ext(basename(.x)), read_sf(.x), envir = .GlobalEnv))
 
-# Heuristic ---------------------------------------------------------------
+#' ## Heuristic
+#' Here we derive a gridded heuristic accounting for RFID signal strength, number of observations and time spent surveying in a given grid cell.
 
 bbox <- st_bbox(
   c(
     xmin = 2598650, xmax = 2599150, # spatial extent of the analysis
     ymin = 1087300, ymax = 1087800
   ),
-  crs = st_crs(2056)
+  crs = st_crs(2056) # EPSG code for swiss CRS
 )
 
-grid <- st_make_grid(bbox, square = F, cellsize = 5) |> # create a grid and number cells
+grid <- st_make_grid(bbox, square = F, cellsize = 5) |> # create a hexagonal grid
   st_as_sf() |>
-  mutate(cell = row_number()) |>
+  mutate(cell = row_number()) |> # number grid cells
   rename(geometry = x)
 
 t_in_cell <- dGNSS |> # find the time roving antenna in each cell per survey
@@ -27,7 +39,7 @@ t_in_cell <- dGNSS |> # find the time roving antenna in each cell per survey
   summarise(t_in_cell = n()) |>
   st_drop_geometry()
 
-heur <- roving_antenna |> # calculate a heuristic accounting for signal strength, n observations and time in cell
+heur <- roving_antenna |> # calculate the heuristic
   st_intersection(grid) |>
   group_by(ID, day, cell) |>
   reframe(
@@ -49,7 +61,25 @@ heur <- roving_antenna |> # calculate a heuristic accounting for signal strength
     y = st_coordinates(geometry)[, 2]
   )
 
-# KDE ---------------------------------------------------------------------
+#'
+#| echo: false
+ggplot() +
+  geom_sf(data = glacier_outline, fill = NA) +
+  geom_sf(data = channel |>
+            st_union() |>
+            st_cast('LINESTRING'),
+          col = 'blue') +
+  geom_tile(data = heur, aes(x, y, fill = factor(day), alpha = heur),
+            width = 10, height = 10) +
+  labs(x = NULL, y = NULL, fill = 'Day', alpha = 'Heuristic') +
+  coord_sf(datum = 2056) +
+  facet_wrap(~ID) +
+  theme_bw() +
+  ggtitle('Gridded heuristic for particle observation strength',
+          subtitle = '2x grid cell scale exaggeration')
+
+#' ## KDE
+#' We then perform a kernel smoothing to estimate the probability density distribution in 2D space of each tagged particle on each survey. The location with the highest probability density (KDE_max) is taken as the particle location, with a 95% confidence interval contour. The along-channel distance of the closest channel point to the KDE_max is computed.
 
 kde <- heur |>
   group_by(ID, day) |>
@@ -91,42 +121,8 @@ injection <- tags |> # add in data for when the particle was injected
   st_as_sf(coords = c("x", "y"), crs = 2056)
 kde_max <- bind_rows(injection, kde_max)
 
-# Stationary antennas -----------------------------------------------------
-
-antenna_range <- antennas |>
-  st_buffer(30) |> # define a detection range of 30 m around each antenna
-  st_intersection(channel) |>
-  group_by(antenna) |>
-  summarise(
-    min_dist = round(min(dist)), # the upper and lower bounds of detection along the channel
-    max_dist = round(max(dist))
-  ) |>
-  st_drop_geometry()
-
-stationary_antennas <- stationary_antennas |>
-  arrange(ID, time, antenna) |>
-  group_by(ID, antenna) |>
-  mutate(duration = lead(time) - time) |> # time particle in range of antenna
-  filter(IO == "In") |>
-  select(time, day, ID, antenna, duration, dep_time) |>
-  filter(time > dep_time) |>
-  left_join(antenna_range) |>
-  mutate(
-    bin = cut(time,
-      breaks = seq(1, 365, 1 / 24), # cut data into hourly bins
-      labels = seq(1, 365 - 1 / 24, 1 / 24)
-    ),
-    bin = as.numeric(paste(bin))
-  ) |>
-  group_by(ID, antenna, bin) |>
-  reframe(
-    duration = sum(duration), # summarise across hourly bins
-    min_dist = min_dist,
-    max_dist = max_dist
-  )
-
-# Plotting ----------------------------------------------------------------
-
+#'
+#| echo: false
 ggplot() +
   geom_sf(
     data = channel |>
@@ -145,6 +141,44 @@ ggplot() +
   labs(col = "Day") +
   ggtitle("Particle location on a given survey with 95% CI")
 
+#' ## Stationary antennas
+#' We then define an estimated horizontal detection range of each stationary antenna using a spatial buffer and determine the upper and lower limits of detection along the subglacial channel. The time particles spent in range of the antennas is calculated and binned by hourly timesteps.
+
+antenna_range <- antennas |>
+  st_buffer(30) |> # define a detection range of 30 m around each antenna
+  st_intersection(channel) |>
+  group_by(antenna) |>
+  summarise(
+    min_dist = round(min(dist)), # the upper and lower bounds of detection along the channel
+    max_dist = round(max(dist))
+  ) |>
+  st_drop_geometry()
+
+stationary_antennas <- stationary_antennas |>
+  arrange(ID, time, antenna) |>
+  group_by(ID, antenna) |>
+  mutate(duration = lead(time) - time) |> # time particle in range of antenna
+  filter(IO == "In") |>
+  select(time, day, ID, antenna, duration, dep_time) |>
+  filter(time > dep_time) |>
+  left_join(antenna_range, by = 'antenna') |>
+  mutate(
+    bin = cut(time,
+      breaks = seq(1, 365, 1 / 24), # cut data into hourly bins
+      labels = seq(1, 365 - 1 / 24, 1 / 24)
+    ),
+    bin = as.numeric(paste(bin))
+  ) |>
+  group_by(ID, antenna, bin) |>
+  reframe(
+    duration = sum(duration), # summarise across hourly bins
+    min_dist = min_dist,
+    max_dist = max_dist
+  )
+
+#' Displaying both the roving antenna KDE_max data (black dots) and stationary antenna data (coloured bars) as a function of along-channel transport distance from the borehole.
+#'
+#| echo: false
 ggplot() +
   geom_rect(
     data = stationary_antennas,
@@ -160,6 +194,7 @@ ggplot() +
   geom_hline(yintercept = 350, linetype = "dashed") +
   facet_wrap(~ID) +
   theme_bw() +
-  ggtitle("Particle transport distance over time with a simple linear model linking roving antenna data",
+  ggtitle("Particle transport distance over time",
     subtitle = "See Jenkin et al. in prep for automated modelling combining roving and stationary antennas"
   )
+
